b357ac933ae15ae88c179ac7f0fc271e46dc489b
ansharma-as <anshsharma712.as@gmail.com>
2025-11-02 04:58:27 +0530

    added allwHost for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
@@ -10,7 +9,8 @@ export default defineConfig({
     port: 5173,
   },
   preview: {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port: 5173,
+    allowedHosts: ['scaler-app-ymat6.ondigitalocean.app'],
   },
 })
